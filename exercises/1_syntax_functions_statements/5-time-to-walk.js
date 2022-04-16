80cba73dee7d28424972fb712771841eb4e10d05
Erin Bedri <erinbedri@gmail.com>
2022-04-16 19:05:47 +0200

    solved exercise 5

---
+++ exercises/1_syntax_functions_statements/5-time-to-walk.js
@@ -0,0 +1,17 @@
+function solve(steps, footprint, speed) {
+    let speedMetersPerSecond = speed * 1000 / 3600;
+    let distance = steps * footprint;
+    let breaks = Math.floor(distance / 500) * 60;
+    let timeSeconds = distance / speedMetersPerSecond + breaks;
+
+    let hours = Math.floor(timeSeconds / 3600);
+    let minutes = Math.floor(timeSeconds / 60);
+    let seconds = Math.ceil(timeSeconds % 60);
+
+    const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+    console.log(`${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)}`)
+}
+
+solve(2564, 0.70, 5.5);
+
