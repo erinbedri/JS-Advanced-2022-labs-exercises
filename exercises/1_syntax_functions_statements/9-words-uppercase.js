ed2cfa48c38aedf64ea267a06f9eb923fb5fb538
Erin Bedri <erinbedri@gmail.com>
2022-04-16 19:52:26 +0200

    solved exercise 9

--- exercises/1_syntax_functions_statements/9-words-uppercase.js
+++ exercises/1_syntax_functions_statements/9-words-uppercase.js
@@ -1,3 +1,12 @@
 function uppercase(input) {
-    
+	let words = input.match(/"[^"]*"|\w+/g);
+    let result = [];
+
+    for (let i = 0; i < words.length; i++) {
+        result.push(words[i].toUpperCase());
+    }
+
+    console.log(result.join(', '));
 }
+
+uppercase('Functions in JS can be nested, i.e. hold other functions')
