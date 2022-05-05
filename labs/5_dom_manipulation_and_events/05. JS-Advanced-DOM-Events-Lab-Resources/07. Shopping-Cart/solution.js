fd684368cd9cd72023a41ca7ccf72c404d3bf086
Erin Bedri <erinbedri@gmail.com>
2022-05-05 16:59:07 +0200

    update to lab exercise

--- labs/5_dom_manipulation_and_events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ labs/5_dom_manipulation_and_events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,11 +1,27 @@
 function solve() {
-   let addButtonElements = Array.from(document.querySelectorAll('.add-product'));
+   let addButtonElements = Array.from(document.getElementsByClassName('add-product'));
    let textAreaElement = document.getElementsByTagName('textarea')[0];
    let checkoutButtonElement = document.getElementsByClassName('checkout')[0];
 
    let totalCost = 0;
    let cart = new Set();
 
+   // Event Delegation
+   document.addEventListener('click', e => {
+      if (e.target.matches('.add-product')) {
+         let currentParentElement = e.target.parentElement.parentElement;
+         let currentProductPrice = Number(currentParentElement.querySelector('.product-line-price').textContent);
+         let currentProductName = currentParentElement.querySelector('.product-title').textContent;
+
+         textAreaElement.disabled = 'false';
+         textAreaElement.textContent += `Added ${currentProductName} for ${currentProductPrice.toFixed(2)} to the cart.\n`;
+
+         totalCost += currentProductPrice;
+         cart.add(currentProductName);
+      }
+   })
+
+   /*
    addButtonElements.forEach(el => {
       el.addEventListener('click', (e) => {
          if (e.target.className == 'add-product') {
@@ -21,6 +37,7 @@ function solve() {
          }
       })
    })
+   */
    
    checkoutButtonElement.addEventListener('click', () => {
       textAreaElement.textContent += `You bought ${Array.from(cart).join(', ')} for ${totalCost.toFixed(2)}.`;
@@ -31,3 +48,12 @@ function solve() {
       checkoutButtonElement.disabled = 'true';
    })
 }
+
+/*
+// Handy Global event Selector - Event Delegation
+function addGlobalEventListener(type, selector, callback) {
+   document.addEventListener(type, e => {
+      if (e.targer.matches(selector)) callback(e)
+   })
+}
+*/
