db61705c79ef780ed900b3280f7697fd38f1e09c
Erin Bedri <erinbedri@gmail.com>
2022-05-05 11:54:49 +0200

    finished lab exercises

--- labs/5_dom_manipulation_and_events/05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ labs/5_dom_manipulation_and_events/05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+
+    const re = /\S+@\S+\.\S+/;
+
+    inputElement.addEventListener('change', (event) => {
+
+        if (re.test(event.target.value)) {
+            event.target.removeAttribute('class');
+            return;
+        }
+        event.target.className = 'error';
+    });
 }
--- labs/5_dom_manipulation_and_events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ labs/5_dom_manipulation_and_events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+   let addButtonElements = Array.from(document.querySelectorAll('.add-product'));
+   let textAreaElement = document.getElementsByTagName('textarea')[0];
+   let checkoutButtonElement = document.getElementsByClassName('checkout')[0];
+
+   let totalCost = 0;
+   let cart = new Set();
+
+   addButtonElements.forEach(el => {
+      el.addEventListener('click', (e) => {
+         if (e.target.className == 'add-product') {
+            let currentParentElement = e.target.parentElement.parentElement;
+            let currentProductPrice = Number(currentParentElement.querySelector('.product-line-price').textContent);
+            let currentProductName = currentParentElement.querySelector('.product-title').textContent;
+
+            textAreaElement.disabled = 'false';
+            textAreaElement.textContent += `Added ${currentProductName} for ${currentProductPrice.toFixed(2)} to the cart.\n`;
+
+            totalCost += currentProductPrice;
+            cart.add(currentProductName);
+         }
+      })
+   })
+   
+   checkoutButtonElement.addEventListener('click', () => {
+      textAreaElement.textContent += `You bought ${Array.from(cart).join(', ')} for ${totalCost.toFixed(2)}.`;
+
+      addButtonElements.forEach(el => {
+         el.disabled = 'true';
+      });
+      checkoutButtonElement.disabled = 'true';
+   })
 }
